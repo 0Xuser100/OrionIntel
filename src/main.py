a45134c6824c5289461ca103f313322e983457b4
mahmoud abdelhamid <mahmoud.abdelhamid@thinktech-it.com>
2026-03-29 00:14:51 +0200

    Refactor code structure for improved readability and maintainability

--- src/main.py
+++ src/main.py
@@ -1,23 +1,48 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from loguru import logger
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from helpers.config import get_settings
 from routes import base, data
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-async def startup_db_client():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     settings = get_settings()
+
+    # Startup
+    logger.info("Application startup: Initializing MongoDB and LLM clients...")
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
+    llm_provider_factory = LLMProviderFactory(settings)
 
-@app.on_event("shutdown")
-async def shutdown_db_client():
-    app.mongo_conn.close()
+    # generation client
+    app.generation_client = llm_provider_factory.create(
+        provider=settings.GENERATION_BACKEND
+    )
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
 
+    # embedding client
+    app.embedding_client = llm_provider_factory.create(
+        provider=settings.EMBEDDING_BACKEND
+    )
+    app.embedding_client.set_embedding_model(
+        model_id=settings.EMBEDDING_MODEL_ID,
+        embedding_size=settings.EMBEDDING_MODEL_SIZE,
+    )
+    try:
+        yield
+    finally:
+        # Shutdown
+        logger.info("Application shutdown: Closing MongoDB connection...")
+        app.mongo_conn.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
---
+++ src/stores/llm/providers/__init__.py
@@ -0,0 +1,2 @@
+from .CoHereProvider import CoHereProvider  # noqa: F401
+from .OpenAIProvider import OpenAIProvider  # noqa: F401
